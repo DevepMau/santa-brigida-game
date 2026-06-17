6d331be6bf9af39dec8feec1a7c30684dca80b1d
DevepMau <oshiettecs@gmail.com>
2026-06-17 14:31:29 -0300

    Agrega nuevos colores de UI y ajusta tamaños y nombres en TemaUI.java
    - Añadidos colores primarios, luz, sombra, dorado, negro y de estado
    - Cambiado tamaño de texto base de 24f a 16f
    - Renombrado parámetro de getMarumonica a "escala"
    - Actualizada multiplicación de tamaño de fuente con nuevo nombre de
    parámetro
    - Mejora la consistencia de nombres y paleta de colores en la UI

--- santaBrigida/src/com/santabrigida/ui/TemaUI.java
+++ santaBrigida/src/com/santabrigida/ui/TemaUI.java
@@ -12,11 +12,21 @@ public class TemaUI {
 	public static final Color COL_HOVER = new Color(122, 132, 148);
 	public static final Color COL_TEXT = Color.WHITE;
 	
+	public static final Color UI_PRIMARIO = new Color(181, 138, 96);
+	public static final Color UI_LUZ = new Color(216, 195, 165);
+	public static final Color UI_SOMBRA = new Color(92, 64, 51);
+	public static final Color UI_DORADO = new Color(255, 155, 55);
+	public static final Color UI_NEGRO = new Color(20, 20, 20);
+	public static final Color UI_TEXTO = new Color(225, 225, 225);
+	public static final Color UI_STAT = new Color(255, 255, 155);
+	public static final Color UI_BUFF = new Color(155, 255, 155);
+	public static final Color UI_DEBUFF = new Color(255, 155, 155);
+	
 	private static Font BASE;
 
     public static final float TAM_TITULO = 40f;
     public static final float TAM_DIALOGO = 34f;
-    public static final float TAM_TEXTO = 24f;
+    public static final float TAM_TEXTO = 16f;
     public static final float TAM_BOTON = 24f;
 
     static {
@@ -31,7 +41,7 @@ public class TemaUI {
         }
     }
 
-    public static Font getMarumonica(float tamano, float escalado) {
-        return BASE.deriveFont(Font.BOLD, tamano * escalado);
+    public static Font getMarumonica(float tamano, float escala) {
+        return BASE.deriveFont(Font.BOLD, tamano * escala);
     }
 }
